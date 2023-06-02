39681e2ed0c943d3c8e02dbf2b60c33981832b22
BilalX7 <BilalX7>
2023-06-03 00:01:00 +0300

    edit

---
+++ src/pages/BookPage/BookPage.jsx
@@ -0,0 +1,18 @@
+import './BookPage.css'
+import styles from '../../style';
+import { Books, Footer }  from '../../components';
+
+const BookPage = () => {
+  return (
+    <>
+      <div className={`bg-inherit ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+            <Books />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default BookPage
